a127ccde4d8e8ca696595f8ea6f906ab65cb5a50
piguet <freeableton@gmail.com>
2019-09-20 14:04:50 +0200

    Update max_exposure.py

--- src/max_exposure.py
+++ src/max_exposure.py
@@ -14,14 +14,7 @@ class LongExposure:
         self.step = step
 
     @staticmethod
-    def averager():
-        """Calculate the average using a clojure."""
-        count = 0
-        total = 0.0
-        squares = [0]
-
-        
-        def averager():
+   def averager():
         """Calculate the average using a clojure."""
         count = 0
         total = 0.0
